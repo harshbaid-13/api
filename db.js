115e05b4ebc1e40437b89dd435093e1b281aba66
am-here <harshbaid.ind@gmail.com>
2023-04-15 23:41:24 +0530

    haha

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql";
 
-
 export const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'ANA@20mika',
-    database:'blog',
+  host: "127.0.0.1",
+  user: "root",
+  port: "https://api-am-here.vercel.app/",
+  password: "ANA@20mika",
+  database: "blog",
 });
 
 //const q  ="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ANA@20mika' ";
